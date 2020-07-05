a0469e87920c00866f4d07981f99151291bdd0e8
Mattia Sanfilippo <imattolo@me.com>
2020-07-05 10:11:17 +0200

    minor fix

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -3,7 +3,7 @@ import * as ActionTypes from './ActionTypes';
 export const Comments = (state = {
     errMess: null,
     comments: []
-}, action) => {
+    }, action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
             return { ...state, isLoading: false, errMess: null, comments: action.payload };
